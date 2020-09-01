cb4a9bc71ad11a9655a48f7cab9a8ff46ef77d43
Andrew <lordbelial15@gmail.com>
2020-09-02 01:39:23 +0300

    Initial commit

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+    'map': {
+        '*': {
+            'app': 'Magepro_SwiperSlider/js/app',
+            'slick': 'Magepro_SwiperSlider/js/swiper'
+        }
+    },
+    'shim': {
+        'Magepro_SwiperSlider/js/swiper': ['jquery']
+    }
+};
